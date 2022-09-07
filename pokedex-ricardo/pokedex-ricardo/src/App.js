db764b6950d3efd570d1cd0de27ff5fe95f989ef
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-09-06 22:08:52 -0300

    segunda modificação

--- pokedex-ricardo/pokedex-ricardo/src/App.js
+++ pokedex-ricardo/pokedex-ricardo/src/App.js
@@ -1,17 +1,27 @@
-
+import React from "react"
 import './App.css';
 import Navbar from './components/NavBar';
 import SearchBar from './components/SearchBar';
+import searchPokemon from './Api';
 
+
+const onSearchHandler = async (pokemon) => {
+  const result = await searchPokemon(pokemon)
+  console.log("pokemon ", result);
+}
 function App() {
   return (
     <div>
       <Navbar/>
-      <SearchBar/>
+      <SearchBar 
+        onSearch={onSearchHandler}
+      />
+
       <div className="App">
         
       </div>
     </div>
+
   );
 }
 
--- pokedex-ricardo/pokedex-ricardo/src/components/SearchBar.js
+++ pokedex-ricardo/pokedex-ricardo/src/components/SearchBar.js
@@ -1,18 +1,26 @@
 import React from "react"
 import { useState } from "react"
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+    const {onSearch} = props
     const [search, setSearch] = useState("dito")
 
     const onChangeHandle = (e) => {
         console.log("pokemon: ", e.target.value);
         setSearch(e.target.value)
     }
+
+    const onButtonClickHandler = () => {
+        onSearch(search);
+    }
     return (
         <div className="searchbar-container">
             <div className="searchbar">
                 <input placeholder="Buscar Pokemon" onChange={onChangeHandle}/>
-                {search}
+            </div>
+
+            <div className="searchbar-btn">
+                <button onClick={onButtonClickHandler} >Buscar</button>
             </div>
         </div>
     )
